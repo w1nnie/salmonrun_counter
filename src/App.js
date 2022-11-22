90429d4b21dc3385fb9741aeef8d4c52f4fd4ea7
w1nnie <winnie.trap@gmail.com>
2022-11-22 13:35:07 +0900

    add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppContainer />
     </div>
   );
 }
+
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {eval: 40};
+  }
+
+  render() {
+    return (
+      <div className="AppContainer">
+        <Display eval = {this.state.eval}/>
+        <Buttons eval = {this.state.eval}/>
+      </div>
+    );
+  }
+}
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="display">
+        {this.props.eval}
+      </div>
+    );
+  }
+}
+
+class Buttons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {eval: 40};
+  }
+
+  handleClick(value) {
+    this.setState({eval: this.state.eval+value});
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="buttons">
+        <div className="grid-wrapper">
+          <button className="box" onClick={() => {this.handleClick(20)}}>+20</button>
+          <button className="box" onClick={() => {this.handleClick(20)}}>+20 ex-O</button>
+          <button className="box" onClick={() => {this.handleClick(20)}}>+20 ex-X</button>
+          <button className="box" onClick={() => {this.handleClick(-20)}}>-20</button>
+          <button className="box" onClick={() => {this.handleClick(-10)}}>-10</button>
+          <button className="box" onClick={() => {this.handleClick(0)}}>±0 </button>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
